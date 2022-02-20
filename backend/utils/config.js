1c66a512e41b1bb2dc899be82fa3844a91510ed3
Miska Kananen <miska.kananen@gmail.com>
2022-02-20 15:58:32 +0200

    Specify backend port

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,7 +4,7 @@ const MONGODB_URL = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URL
   : process.env.MONGODB_URL
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3003
 
 module.exports = {
   MONGODB_URL,
